256c37447421228642649e525aa721dec7443e9e
serhiy <sergiy.forkalyuk@gmail.com>
2020-04-16 16:40:23 +0300

    Добавлено приложение для списков с намеренно неработающим модульным тестом

---
+++ lists/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+
+# Create your tests here.
+
+class SmokeTest(TestCase):
+    """тест на токсичность"""
+
+    def test_bad_maths(self):
+        """тест: неправильные математические расчеты"""
+        self.assertEqual(1 + 1, 3)
