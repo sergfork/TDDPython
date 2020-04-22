a486350709396088b6e60ad11727f8c5e2b40471
serhiy <sergiy.forkalyuk@gmail.com>
2020-04-22 20:47:34 +0300

    Using Bootstrap to improve a maket

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -6,7 +6,7 @@ import time
 
 
 MAX_WAIT = 10
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """тест нового посетителя"""
 
     def setUp(self):
@@ -30,50 +30,6 @@ class NewVisitorTest(LiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
-
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        """тест: можно начать список и получить его позже"""
-        # Эдит  слышала про крутое новое онлайн-приложение со списком
-        # неотложных дел. Она решает оценить его домашеюю страницу
-        self.browser.get(self.live_server_url)
-
-        # Она видит, что заголовок и шапка страницы говорят о списках
-        # неотложных дел
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
-
-        # Ей сразу же предлагается ввести элемент списка
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
-
-        # Она набирает в текстовом поле "Купить павлиньи перья" ( ее хобби -
-        # вязание рыболовных мушек
-        inputbox.send_keys('Купить павлиньи перья')
-
-        # Когда она нажимает enter, страница обновляется, и теперь страница
-        # содержит "1: Купить павлиньи перья" в качестве элемента списка
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
-
-        # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
-        # Она вводит "Сделать мушку из павлиньих перьев"
-        # (Эдит очень методична)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Сделать мушку из павлиньих перьев')
-        inputbox.send_keys(Keys.ENTER)
-
-        # Страница снова обновляется, и теперь показывает оба элемента ее списка
-        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
-        self.wait_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
-
-        # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
-        # сайт сгенерировал для нее уникальный URL-адрес - об этоом
-        # выводится небольшой текст с объяснениями.
-
-        # Она посещает этот URL-адрес - ее список по-прежнему там.
-
-        # Удовлетворенная, она снова ложится спать
 
     def test_can_start_a_list_for_one_user(self):
         """test: can start list for one user"""
